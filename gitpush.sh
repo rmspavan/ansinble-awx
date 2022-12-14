ca7a9803f89181b2e488fb543af8075a834a227e
rmspavan <rmspavan@outlook.com>
2022-12-14 16:49:31 +0800

    update ansible playbook

---
+++ gitpush.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd /root/github/ansinble-awx 
+git add .
+git commit -m "update ansible playbook"
+git push 
+#mygitusername root
+#echo Press Enter...
+#mygitpassword password
+#echo Press Enter... 
